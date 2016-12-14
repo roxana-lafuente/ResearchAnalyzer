558c1a88a9b18013b64a9812fdad25f90330b387
miguelemosreverte <miguellemos95@gmail.com>
2016-12-14 14:34:41 -0300

    As promised, now it creates groups like Left Clicks or Right Clicks, and sorts clicks as expected on visualization.

--- visualizer.py
+++ visualizer.py
@@ -15,6 +15,11 @@ li = LogInfo(path + "click_images/clickimagelogfile_zxysp.txt", # Your click dat
              path + "detailed_log/detailedlogfile_zxysp.txt", # Your detailed log file here
              path + "timed_screenshots/timedscreenshootlogfile_zxysp.txt", # Your timed screenshot log file here
              path + "system_log/system_log_zxysp.txt") # Your system log data here
+
+stylized_group_names = {"left":"Left Clicks","right":"Right Clicks"}
+group_names = ["left", "right"]
+
+print li.get_click_info()
 # Print clicks summary info.
 neccesary_clicks_information = []
 for id,clickinfo in  enumerate(li.get_click_info()):
@@ -22,14 +27,15 @@ for id,clickinfo in  enumerate(li.get_click_info()):
     start_timestamp = str(datetime.datetime.fromtimestamp(float(clickinfo[1][0])/1000.0))
     end_timestamp = str(datetime.datetime.fromtimestamp(float(clickinfo[2])/1000.0))
 
+    click_type = clickinfo[0]
+
     neccesary_click_information = {}
     neccesary_click_information["id"] = id
     neccesary_click_information["content"] = "item " + str(id) #+ ' <span style="color:#97B0F8;">(' + names[id % 2] + ')</span>'
     neccesary_click_information["start"] = start_timestamp
     neccesary_click_information["end"] = end_timestamp
-    neccesary_click_information["group"] = id % 2
+    neccesary_click_information["group"] = group_names.index(click_type)
     #neccesary_click_information["type"] = "box"
     neccesary_clicks_information.append(neccesary_click_information)
-group_names = ["First", "Second", "Third", "Fourth"]
-inject_into_html(json.dumps(neccesary_clicks_information),group_names,"clicks")
+inject_into_html(json.dumps(neccesary_clicks_information),stylized_group_names.values(),"clicks")
 webbrowser.open("visualization/clicks.html",new=2)
