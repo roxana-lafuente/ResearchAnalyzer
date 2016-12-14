8f5008ebaa53b8e3a59196f637e9c7e28b8fd924
miguelemosreverte <miguellemos95@gmail.com>
2016-12-14 16:42:01 -0300

    Fixed the dates problem

--- visualizer.py
+++ visualizer.py
@@ -19,14 +19,17 @@ li = LogInfo(path + "click_images/clickimagelogfile_zxysp.txt", # Your click dat
 stylized_group_names = {"left":"Left Clicks","right":"Right Clicks"}
 group_names = ["left", "right"]
 
-print li.get_click_info()
+print li.get_click_info_for_visualization()
 # Print clicks summary info.
 neccesary_clicks_information = []
-for id,clickinfo in  enumerate(li.get_click_info()):
-    #TODO fix the timestamps, for some reason they give me dates like 1969 when the current year is 2016! I feel like I am taking crazy pills!
-    start_timestamp = str(datetime.datetime.fromtimestamp(float(clickinfo[1][0])/1000.0))
-    end_timestamp = str(datetime.datetime.fromtimestamp(float(clickinfo[2])/1000.0))
-
+for id,clickinfo in  enumerate(li.get_click_info_for_visualization()):
+    start_relative_timestamp = float(clickinfo[1][0])
+    end_relative_timestamp = float(clickinfo[2])
+    date =  datetime.datetime.strptime(str(clickinfo[3]), "%Y%m%d").date()
+    timestamps_offset = float(date.strftime("%s")) * 1000
+    start_timestamp = str(datetime.datetime.fromtimestamp((start_relative_timestamp + timestamps_offset)/1000.0))
+    end_timestamp = str(datetime.datetime.fromtimestamp((end_relative_timestamp + timestamps_offset)/1000.0))
+    print start_timestamp
     click_type = clickinfo[0]
 
     neccesary_click_information = {}
@@ -35,7 +38,7 @@ for id,clickinfo in  enumerate(li.get_click_info()):
     neccesary_click_information["start"] = start_timestamp
     neccesary_click_information["end"] = end_timestamp
     neccesary_click_information["group"] = group_names.index(click_type)
-    #neccesary_click_information["type"] = "box"
+    neccesary_click_information["type"] = "box"
     neccesary_clicks_information.append(neccesary_click_information)
 inject_into_html(json.dumps(neccesary_clicks_information),stylized_group_names.values(),"clicks")
 webbrowser.open("visualization/clicks.html",new=2)
