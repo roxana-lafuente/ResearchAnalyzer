816ba4a9b28e6db2af5d1dc7ee992608d119a05a
miguelemosreverte <miguellemos95@gmail.com>
2016-12-15 17:08:58 -0300

    Added the function prepare_sentence_for_HTML which for now only changes the appearances of return to the HTML entity for the return symbol. Only if return is not preceded by whitespace, that is.

--- visualizer.py
+++ visualizer.py
@@ -47,23 +47,32 @@ class Visualizer():
 
         self.injector.prepareForHTML(json.dumps(neccesary_clicks_information),stylized_group_names.values(),"clicks")
 
-    def parse_and_inject_sentences_into_HTML(self):
-        stylized_group_names = {"sentences":"Sentences written in general","google_chrome":"Sentences written in Google Chrome"}
-        self.group_names.extend(["google_chrome", "sentences"])
+    def prepare_sentence_for_HTML(self, sentence):
+        index_of_return_word = sentence.find("return")
+        is_return_preceded_by_whitespace = sentence[index_of_return_word-1:index_of_return_word] == " "
+        if not is_return_preceded_by_whitespace:
+            sentence = sentence.replace("return","&#9166")
+        return sentence
 
+    def parse_and_inject_sentences_into_HTML(self):
         neccesary_sentences_information = []
+        stylized_group_names = {}
         for sentence_info in  li.get_clustered_keys():
             self.global_id += 1
 
             start_timestamp = sentence_info[1]
             end_timestamp = sentence_info[2]
+            window_name = sentence_info[3]
+            if window_name not in self.group_names:
+                self.group_names.append(window_name)
+                stylized_group_names[window_name] = "Sentences written in " + window_name
 
             neccesary_sentence_information = {}
             neccesary_sentence_information["id"] = self.global_id
-            neccesary_sentence_information["content"] = ' <span style="color:#97B0F8;">(' + sentence_info[0] + ')</span>'
+            neccesary_sentence_information["content"] = ' <span style="color:#97B0F8;">(' + self.prepare_sentence_for_HTML(sentence_info[0]) + ')</span>'
             neccesary_sentence_information["start"] = start_timestamp
             neccesary_sentence_information["end"] = end_timestamp
-            neccesary_sentence_information["group"] = self.group_names.index("sentences")
+            neccesary_sentence_information["group"] = self.group_names.index(window_name)
             neccesary_sentence_information["type"] = "box"
             neccesary_sentences_information.append(neccesary_sentence_information)
         self.injector.prepareForHTML(json.dumps(neccesary_sentences_information),stylized_group_names.values(),"sentences")
@@ -81,3 +90,4 @@ if __name__ == "__main__":
                  path + "timed_screenshots/timedscreenshootlogfile_zxysp.txt", # Your timed screenshot log file here
                  path + "system_log/system_log_zxysp.txt") # Your system log data here
     vis = Visualizer()
+    #print li.get_orientation_info()
